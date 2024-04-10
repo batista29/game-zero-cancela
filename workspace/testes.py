2cc28c43794ed8d0daecbe1d581c1065313b20d1
esterAmaral <163193914+esterAmaral@users.noreply.github.com>
2024-04-10 11:11:30 -0300

    Add files via upload
    arrumar o resultado quando digita o primeiro 0

--- workspace/testes.py
+++ workspace/testes.py
@@ -1,28 +1,29 @@
 # depois ver o que fazer quando o primeiro numero digitado é 0
 #lucia disse para imprimir que não há numero digitado
 #ler número
-
-ultimo = penultimo = antepenultimo = soma = numero = 0
+atual = 0
+soma = 0
+ultimo = penultimo = antepenultimo = 0
 qdt_considerados = qdt_desconsiderados = qdt_numeros = 0
 
 # laço de repetição
-while(numero >= 0):
-    antepenultimo = penultimo
-    penultimo = ultimo
-
-    ultimo = int(input("Digite um número? "))
-    soma = soma + ultimo
-
-    if(soma == 0):
-        print("Não há nenhum número digitado!")
-    else:
-        if(ultimo == 0):
+while(atual >= 0):
+        if(atual != 0):
+             antepenultimo = penultimo
+             penultimo = ultimo
+             ultimo = atual
+        elif(atual == 0):
+            
             soma = soma - ultimo
             ultimo = penultimo
             penultimo = antepenultimo
-            antepenultimo = -1
-            
-    print(ultimo)
-    print(penultimo)
-    print(antepenultimo)
-    print(soma)
+            antepenultimo = 0
+        atual = int(input("Digite um número? "))
+        soma = soma + atual
+        if(soma == 0):
+            print("Não há nenhum número digitado!")
+        print("O ultimo numero", ultimo)
+        print("O penultimo numero", penultimo)
+        print("O antepenultimo numero", antepenultimo)
+        print("soma ", soma)
+       
