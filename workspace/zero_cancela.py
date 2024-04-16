50712c44ca987368ebed0f1d7bffe98a7528edd8
esterAmaral <163193914+esterAmaral@users.noreply.github.com>
2024-04-16 19:20:17 -0300

    ultima versão zero cancela

--- workspace/zero_cancela.py
+++ workspace/zero_cancela.py
@@ -1,53 +1,49 @@
-# depois ver o que fazer quando o primeiro número digitado for 0
-#lucia disse para imprimir que não há número digitalizado
-# Código está funcionando
-# falta contabilizar os números considerados e os desconsiderados
-#ler número
-atual  =  0
 soma  =  0
 ultimo  =  penultimo  =  antepenultimo  =  pre_antepenultimo =  0
-ultimo =  penultimo  =  antepenultimo  =  pre_antepenultimo =  0
-aux_ultimo  =  aux_penultimo  =  aux_antepenultimo  =  aux_pre_antepenultimo  =  0
+aux_ultimo  =  aux_penultimo  =  aux_antepenultimo  = aux_pre_antepenultimo =  1
 qtd_considerados  =  qtd_desconsiderados  =  qtd_numeros  =  0
-
+print("Bem-vindo ao Jogo Zero Cancela!")
+#ler número
+atual  = int ( input ( "Digite um número:" ))
 # laço de repetição
 while ( atual  >=  0 ):
-  #atualiza as variaveis desse modo quando o nº atual não for zero
-    if ( atual  !=  0 ):
-        pre_antepenultimo  =  antepenultimo
-        antepenultimo  =  penultimo
-        penultimo  =  ultimo
-        ultimo  =  atual
     # variáveis ​​que controlam os zeros que são digitados
     aux_pre_antepenultimo  =  aux_antepenultimo
     aux_antepenultimo  =  aux_penultimo
     aux_penultimo  =  aux_ultimo
     aux_ultimo  =  atual
-    #recebe número atual
-    atual  =  int ( input ( "Digite um número: " ))
-    # condicinal para verificar se é hr de para o jogo
-    if (atual < 0):
-        break
-    else :
+  #atualiza as variaveis desse modo quando o nº atual não for zero
+    if ( atual  >  0 ):
+        pre_antepenultimo  =  antepenultimo
+        antepenultimo  =  penultimo
+        penultimo  =  ultimo
+        ultimo  =  atual
         soma  =  soma  +  atual
-    if (atual>0):
-         qtd_considerados=qtd_considerados+1
+        qtd_considerados=qtd_considerados+1
     #condicinal para retirar os números digitados errados
-    if ( atual  ==  0 ):
+    elif ( atual  ==  0 ):
         qtd_desconsiderados=qtd_desconsiderados+1
         qtd_considerados=qtd_considerados-1
+        #verifica se ainda tem numero para ser retirado
+        if(soma == 0):
+             print("Atenção: Não há números para serem descartados!")
+             qtd_considerados = 0
+             qtd_desconsiderados -= 1
+        else:
         #condicinal analisar se o usuário digitou mais de 3 zeros
-        if ( atual ==  0  and  aux_ultimo  ==  0  and  aux_penultimo  ==  0  and  aux_antepenultimo  ==  0 ):
-                qtd_desconsiderados=qtd_desconsiderados-1
-                qtd_considerados=qtd_considerados+1
-                print ( "Atenção: NÃO é permitido inserir mais de três 0 consecutivos!" )
-        else :
-         #atualiza as variaveis desse modo quando o nº atual for zero
-            soma  =  soma  -  ultimo
-            ultimo  =  penultimo
-            penultimo  =  antepenultimo
-            antepenultimo  =  pre_antepenultimo        
+            if (aux_ultimo  ==  0  and  aux_penultimo  ==  0  and  aux_antepenultimo  ==  0 and aux_pre_antepenultimo == 0):
+                    qtd_desconsiderados=qtd_desconsiderados-1
+                    qtd_considerados=qtd_considerados+1
+                    print ( "Atenção: NÃO é permitido inserir mais de três 0 consecutivos!" )
+            else :
+            #atualiza as variaveis desse modo quando o nº atual for zero
+                soma  =  soma  -  ultimo
+                ultimo  =  penultimo
+                penultimo  =  antepenultimo
+                antepenultimo  =  pre_antepenultimo  
+     #recebe número atual
+    atual  = int ( input ( "Digite um número:" ))              
 
-print ("Zero Cancelamento Finalizado!" )
-print ("Soma total: ", soma )
-print (f"A quantidade de números considerados é {qtd_considerados} e a quantidade de números desconsiderados é de {qtd_desconsiderados} ")
+print ( "Zero Cancela Finalizado!" )
+print ( "Soma total= " , soma )
+print (f"Números considerados = {qtd_considerados} \nNúmeros desconsiderados = {qtd_desconsiderados} ")
